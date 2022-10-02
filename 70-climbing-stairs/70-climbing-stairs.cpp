a0e6c3bd063b8b05d18d2e8b6c5ce2d3248e78be
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-02 14:43:51 +0530

    Time: 2 ms (43.11%), Space: 5.9 MB (56.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n <= 3) return n;
-        int array[n];
+        int array[45];
         array[0] = 1, array[1] = 2;
-        for(int i = 2; i < n; i++) {
+        for(int i = 2; i < 45; i++) {
             array[i] = array[i - 1] + array[i - 2];
         }
         return array[n - 1];
