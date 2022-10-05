220670dddfdcbce9dab53d5d2ebf26e5d01df6c9
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-05 18:16:50 +0530

    Time: 12 ms (8.80%), Space: 5.9 MB (28.00%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n <= 0) return false;
-        if((n & n - 1) == 0) {
-            if(__builtin_ctz(n) % 2 == 0) return true;
+        if((n & n - 1) == 0 && __builtin_ctz(n) % 2 == 0) {
+            return true;
         }
         return false;
     }
