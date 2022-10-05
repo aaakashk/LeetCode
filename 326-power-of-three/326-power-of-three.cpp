79e7757876803d2452d23398923539c7fca49c9d
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-05 23:11:45 +0530

    Time: 106 ms (5.06%), Space: 13.7 MB (8.58%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n < 1) return false;
+        if(n == 1) return true;
+        map<int, int> map;
+        map[3] = 1;
+        map[9] = 2;
+        map[7] = 3;
+        map[1] = 4;
+        int last_digit = n % 10;
+        if(!map[last_digit]) return false;
+        int power = map[last_digit];
+        double apparent_number = pow(3, power);
+        while(apparent_number <= n) {
+            if(apparent_number == n) return true;
+            power += 4;
+            apparent_number = pow(3, power);
+        }
+        return false;
+    }
+};
