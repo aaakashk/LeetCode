16c648a7a0e78a9ddad64d2d73ad08965ae9ae8b
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-08 21:56:24 +0530

    Time: 5 ms (35.29%), Space: 6.4 MB (30.47%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        int countBars = 0, asterisks = 0;
-        for(int i = 0; i < s.length(); ++i) {
-            if(s[i] == '|') ++countBars;
-            if(countBars % 2 == 0) {
-                if(s[i] == '*') ++asterisks;
-            }
+        int sign = 1, asterisks = 0;
+        for(char& c: s) {
+            if((sign ^= c == '|') && c == '*') ++asterisks;
         }
         return asterisks;
     }
