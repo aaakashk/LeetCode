0a03cdf0be0e3cd9189fd8efa1b595f1d968638c
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-08 21:33:53 +0530

    Time: 3 ms (83.76%), Space: 6.7 MB (27.45%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -8,9 +8,16 @@ public:
         // }
         // return t[t.length() - 1];
         
+        // addition of ASCII values of characters and then subtracting
+        // int result = 0;
+        // for(char c: t) result += c;
+        // for(char c: s) result -= c;
+        // return (char)result;
+        
+        // using XOR
         int result = 0;
-        for(char c: t) result += c;
-        for(char c: s) result -= c;
-        return (char)result;
+        for(char c: t) result ^= c;
+        for(char c: s) result ^= c;
+        return result;
     }
 };
