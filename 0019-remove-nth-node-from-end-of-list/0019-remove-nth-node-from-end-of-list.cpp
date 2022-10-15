46bc1782fd22af98b21066c49daf8db7757c1172
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-15 23:23:55 +0530

    Time: 4 ms (81.25%), Space: 10.8 MB (32.12%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,52 +11,52 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // if(!head->next && n == 1) {
-        //     delete head;
-        //     return nullptr;
-        // }
-        // int count = 1;
-        // ListNode* temp = head, *prev = nullptr;
-        // while(temp->next) {
-        //     count++;
-        //     prev = temp;
-        //     temp = temp->next;
-        // }
-        // if(n == 1) {
-        //     prev->next = nullptr;
-        //     delete temp;
-        //     return head;
-        // }
-        // if(count == n) {
-        //     ListNode* t = head;
+        if(!head->next && n == 1) {
+            delete head;
+            return nullptr;
+        }
+        int count = 1;
+        ListNode* temp = head, *prev = nullptr;
+        while(temp->next) {
+            count++;
+            prev = temp;
+            temp = temp->next;
+        }
+        if(n == 1) {
+            prev->next = nullptr;
+            delete temp;
+            return head;
+        }
+        if(count == n) {
+            ListNode* t = head;
+            head = head->next;
+            delete t;
+            return head;
+        }
+        n = count - n;
+        temp = head;
+        while(n--) {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = temp->next;
+        delete temp;
+        return head;
+        
+        
+        // ListNode prehead(-1, head);
+        // ListNode* prev = &prehead;
+        // for (int i = 0; i < n; ++i) {
         //     head = head->next;
-        //     delete t;
-        //     return head;
         // }
-        // temp = head;
-        // while(count--) {
-        //     prev = temp;
-        //     temp = temp->next;
+        // while (head) {
+        //     head = head->next;
+        //     prev = prev->next;
         // }
-        // prev->next = temp->next;
-        // delete temp;
-        // return head;
-        
-        
-        ListNode prehead(-1, head);
-        ListNode* prev = &prehead;
-        for (int i = 0; i < n; ++i) {
-            head = head->next;
-        }
-        while (head) {
-            head = head->next;
-            prev = prev->next;
-        }
-        ListNode* rip = prev->next;
-        prev->next = prev->next->next;
-        // Don't be that person that leaks memory. :)
-        delete rip;
-        return prehead.next;
+        // ListNode* rip = prev->next;
+        // prev->next = prev->next->next;
+        // delete rip;
+        // return prehead.next;
     
     }
 };
