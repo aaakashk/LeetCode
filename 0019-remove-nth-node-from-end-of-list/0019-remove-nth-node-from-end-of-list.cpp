04542c1dffd625fe66e1d5eecd5ba526b8cc5786
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-10-15 22:52:42 +0530

    Time: 7 ms (67.16%), Space: 10.6 MB (74.83%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // if(!head->next && n == 1) {
+        //     delete head;
+        //     return nullptr;
+        // }
+        // int count = 1;
+        // ListNode* temp = head, *prev = nullptr;
+        // while(temp->next) {
+        //     count++;
+        //     prev = temp;
+        //     temp = temp->next;
+        // }
+        // if(n == 1) {
+        //     prev->next = nullptr;
+        //     delete temp;
+        //     return head;
+        // }
+        // if(count == n) {
+        //     ListNode* t = head;
+        //     head = head->next;
+        //     delete t;
+        //     return head;
+        // }
+        // temp = head;
+        // while(count--) {
+        //     prev = temp;
+        //     temp = temp->next;
+        // }
+        // prev->next = temp->next;
+        // delete temp;
+        // return head;
+        
+        
+        ListNode prehead(-1, head);
+        ListNode* prev = &prehead;
+        for (int i = 0; i < n; ++i) {
+            head = head->next;
+        }
+        while (head) {
+            head = head->next;
+            prev = prev->next;
+        }
+        ListNode* rip = prev->next;
+        prev->next = prev->next->next;
+        // Don't be that person that leaks memory. :)
+        delete rip;
+        return prehead.next;
+    
+    }
+};
