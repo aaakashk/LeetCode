6d6ca4d025620913a15f14a02bd2271cef5089d0
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-11-07 19:51:26 +0530

    Time: 13 ms (92.80%), Space: 15.9 MB (41.99%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,12 +1,39 @@
 class Solution {
 public:
+    // using logic of Moore's Voting Algorithm
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> map;
-        vector<int> vec;
-        for(int i : nums) map[i]++;
-        for(auto& [key, value] : map) {
-            if(value > nums.size() / 3) vec.push_back(key);
+        int count1 = 0, count2 = 0, firstCandidate = 0, secondCandidate = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] == firstCandidate) count1++;
+            else if(nums[i] == secondCandidate) count2++;
+            else if(count1 == 0) {
+                count1++;
+                firstCandidate = nums[i];
+            }
+            else if(count2 == 0) {
+                count2++;
+                secondCandidate = nums[i];
+            }
+            else count1--, count2--;
         }
+        count1 = count2 = 0;
+        vector<int> vec;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] == firstCandidate) count1++;
+            else if(nums[i] == secondCandidate) count2++;
+        }
+        if(count1 > nums.size() / 3) vec.push_back(firstCandidate);
+        if(count2 > nums.size() / 3) vec.push_back(secondCandidate);
         return vec;
     }
+    
+        // vector<int> majorityElement(vector<int>& nums) {
+        // unordered_map<int, int> map;
+        // vector<int> vec;
+        // for(int i : nums) map[i]++;
+        // for(auto& [key, value] : map) {
+        //     if(value > nums.size() / 3) vec.push_back(key);
+        // }
+        // return vec;  
+        //}
 };
