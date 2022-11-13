4cf695985fccd304bd10102cb3f156b6ca890af2
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-11-13 17:34:32 +0530

    Time: 7 ms (16.67%), Space: 6.2 MB (71.77%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
+    
+    string removingHash(string s) {
         int i = 0;
         for(char c : s) {
             if(c != '#') s[i++] = c;
-            else if(i > 0)i--;
+            else i = max(0, --i);
         }
-        string new_s = s.substr(0, i);
-        i = 0;
-        for(char c : t) {
-            if(c != '#') t[i++] = c;
-            else if(i > 0) i--;
-        }
-        string new_t = t.substr(0, i);
-        return new_s == new_t;
+        return s.substr(0, i);
+    }
+    
+    bool backspaceCompare(string s, string t) {
+        return removingHash(s) == removingHash(t);
     }
 };
