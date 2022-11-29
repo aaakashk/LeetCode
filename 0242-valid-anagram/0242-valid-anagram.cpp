dee1ed650ba9fd5651f8e5f10f19dc911e2a14c0
aaakashk <92650528+aaakashk@users.noreply.github.com>
2022-11-29 23:19:01 +0530

    Time: 24 ms (43.87%), Space: 7.4 MB (55.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int arrayt[26], arrays[26];
-        memset(arrayt, 0, sizeof(arrayt));
-        memset(arrays, 0, sizeof(arrays));
-        for(int i = 0; i < s.length(); i++) {
-            arrays[s[i] - 'a']++;
-        }
-        for(int i = 0; i < t.length(); i++) {
-            arrayt[t[i] - 'a']++;
-        }
-        for(int i = 0; i < 26; i++) {
-            if(arrayt[i] != arrays[i]) return false;
+        // int arrayt[26], arrays[26];
+        // memset(arrayt, 0, sizeof(arrayt));
+        // memset(arrays, 0, sizeof(arrays));
+        // for(int i = 0; i < s.length(); i++) {
+        //     arrays[s[i] - 'a']++;
+        // }
+        // for(int i = 0; i < t.length(); i++) {
+        //     arrayt[t[i] - 'a']++;
+        // }
+        // for(int i = 0; i < 26; i++) {
+        //     if(arrayt[i] != arrays[i]) return false;
+        // }
+        // return true;
+        
+        // better way for almost same logic
+        map<char, int> maps, mapt;
+        for(char c : s) maps[c]++;
+        for(char c : t) mapt[c]++;
+        if(maps.size() != mapt.size()) return false;
+        for(auto& i : maps) {
+            if(mapt[i.first] != maps[i.first]) return false;
         }
         return true;
     }
