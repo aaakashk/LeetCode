af79f8450149583c02e0242dc226c13f59a1e6d8
Akash Kumar <92650528+aaakashk@users.noreply.github.com>
2023-10-08 22:58:45 +0530

    Time: 132 ms (30.63%), Space: 96.9 MB (35.44%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        bool increasing = true, decreasing = true;
-        for(int i = 0; i < nums.size() - 1; ++i) {
-            if (nums[i] > nums[i + 1]) increasing = false;
-            else if (nums[i] < nums[i + 1]) decreasing = false;
+        // bool increasing = true, decreasing = true;
+        // for(int i = 0; i < nums.size() - 1; ++i) {
+        //     if (nums[i] > nums[i + 1]) increasing = false;
+        //     else if (nums[i] < nums[i + 1]) decreasing = false;
+        // }
+        // return increasing || decreasing;
+        
+        int direction = 0;
+        for(int i = 1; i < nums.size(); ++i) {
+            if(direction > 0 && nums[i] < nums[i - 1]) return false;
+            if(direction < 0 && nums[i] > nums[i - 1]) return false;
+            if(direction == 0) direction = nums[i] - nums[i - 1];
         }
-        return increasing || decreasing;
+        return true;
     }
 };
