4d56a8be173171cab6e6ef68134e724081a1f32c
Akash Kumar <92650528+aaakashk@users.noreply.github.com>
2023-11-17 18:55:50 +0530

    Time: 2 ms (52.65%), Space: 9.2 MB (79.41%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
@@ -3,10 +3,8 @@ public:
     int countHillValley(vector<int>& nums) {
         int count = 0, last = nums[0];
         for (int i = 1; i < nums.size() - 1; ++i) {
-            if(nums[i] == last) {
-                continue;
-            }
-            else if ((nums[i] > last && nums[i] > nums[i + 1]) || (nums[i] < last && nums[i] < nums[i + 1])) {
+            
+            if ((nums[i] > last && nums[i] > nums[i + 1]) || (nums[i] < last && nums[i] < nums[i + 1])) {
                 last = nums[i];
                 count++;
             }
